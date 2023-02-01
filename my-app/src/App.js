33df842753015d7af741f7d2fb84c1e8472d0dcf
donghyeon <wlalqjrj@gmail.com>
2023-02-01 20:13:22 +0900

    2.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,59 +1,121 @@
-// import Button from "./Button";
-// import styles from "./App.module.css";
-
 import { useState, useEffect } from "react";
-
-// function Hello() {
-//   useEffect(() => {
-//     console.log("Im here!");
-//   });
-//   return <h1>Hello</h1>;
-// }
-
+import Movie from "./components/Movie";
 // function App() {
-//   const [showing, setShowing] = useState(false);
-//   const onClick = () => setShowing((prev) => !prev);
+//   const [toDo, setToDo] = useState("");
+//   const [toDos, setToDos] = useState([]);
+//   const onChange = (event) => setToDo(event.target.value);
+//   const onSubmit = (event) => {
+//     event.preventDefault();
+//     if (toDo === "") {
+//       return;
+//     }
+//     setToDo("");
+//     setToDos((currentArray) => [toDo, ...currentArray]);
+//   };
+//   console.log(toDos);
 //   return (
 //     <div>
-//       {showing ? <Hello /> : null}
-//       <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+//       <h1>My To Dos({toDos.length})</h1>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={toDo}
+//           type="text"
+//           placeholder="Write your to do..."
+//         />
+//         <button>Add To Do</button>
+//       </form>
+//       <hr />
+//       <ul>
+//         {toDos.map((item, index) => (
+//           <li key={index}>{item}</li>
+//         ))}
+//       </ul>
 //     </div>
 //   );
 // }
 
+// export default App;
+
+// 코인 트래커
+
+// function App() {
+//   const [loading, setLoading] = useState(true);
+//   const [coins, setCoins] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.coinpaprika.com/v1/tickers")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         setCoins(json);
+//         setLoading(false);
+//       });
+//   }, []);
+//   return (
+//     <div>
+//       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+//       {loading ? (
+//         <strong>Loading...</strong>
+//       ) : (
+//         <select>
+//           {coins.map((coin) => (
+//             <option>
+//               {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+//             </option>
+//           ))}
+//         </select>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 영화 앱 1
+// function App() {
+//   const [loading, setLoading] = useState(true);
+//   const [movies, setMovies] = useState([]);
+//   const getMovies = async () => {
+//     const json = await (
+//       await fetch(
+//         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+//       )
+//     ).json();
+//     setMovies(json.data.movies);
+//     setLoading(false);
+//   };
+
+//   useEffect(() => {
+//     getMovies();
+//   }, []);
+//   return (
+//     <div>
+//       {loading ? (
+//         <h1>Loading...</h1>
+//       ) : (
+//         <div>
+//           {movies.map((movie) => (
+//             <div key={movie.id}>
+//               <img src={movie.medium_cover_image} />
+//               <h2>{movie.title}</h2>
+//               <p>{movie.summary}</p>
+//               <ul>
+//                 {movie.genres.map((g) => (
+//                   <li key={g}>{g}</li>
+//                 ))}
+//               </ul>
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//영화앱 2
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-
-  useEffect(() => {
-    console.log("CALL THE API.....");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Search for", keyword);
-    }
-  }, [keyword]);
-
-  return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me </button>
-    </div>
-    //   // <div>
-    //   //   <h1 className={styles.title}>Welcome back!</h1>
-    //   //   <Button text={"Continue"} />
-    //   //   <Button className={styles.btn} text={"red"} />
-    //   // </div>
-  );
+  return null;
 }
 
 export default App;
